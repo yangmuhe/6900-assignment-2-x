dad9546446ecaacbce0c67d6106c46fd72fad3ab
siqister <siqi.zhu@gmail.com>
2016-02-24 23:47:07 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('Homework 2-A...')
+
+d3.csv('../data/hubway_trips_reduced.csv',parse,dataLoaded);
+
+function dataLoaded(err,rows){
+
+    console.log(rows);
+
+
+}
+
+function parse(d){
+    if(+d.duration<0) return;
+
+    return {
+        duration: +d.duration,
+        startTime: parseDate(d.start_date),
+        endTime: parseDate(d.end_date),
+        startStation: d.strt_statn,
+        endStation: d.end_statn
+    }
+}
+
+function parseDate(date){
+    var day = date.split(' ')[0].split('/'),
+        time = date.split(' ')[1].split(':');
+
+    return new Date(+day[2],+day[0]-1, +day[1], +time[0], +time[1]);
+}
+
