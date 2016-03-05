83c355e8be57a61537d3985b0d43a05672d9ed93
siqister <siqi.zhu@gmail.com>
2016-03-05 09:59:46 -0500

    Readme

--- script.js
+++ script.js
@@ -4,8 +4,47 @@ d3.csv('../data/hubway_trips_reduced.csv',parse,dataLoaded);
 
 function dataLoaded(err,rows){
 
-    console.log(rows);
+    var cf = crossfilter(rows),
+        tripsByTime = cf.dimension(function(d){return d.startTime}),
+        tripsByAge = cf.dimension(function(d){return d.userAge}),
+        tripsByDuration = cf.dimension(function(d){return d.duration}),
+        tripsByStartStation = cf.dimension(function(d){return d.startStation});
 
+    //Filtering
+
+    //How many trips in 2012 only?
+    tripsByTime.filter([new Date(2012,0,1),new Date(2012,11,31)]);
+    console.log(tripsByTime.top(Infinity).length);
+
+    //How many trips in 2012 are by those under 18?
+    tripsByAge.filter([.1,18]);
+    console.log(tripsByAge.top(Infinity));
+    console.log(tripsByTime.top(Infinity));
+
+    //How many trips in 2012 are by those over 60?
+    tripsByAge.filter(function(d){return d<20 || d>40});
+    console.log(tripsByAge.top(Infinity).length);
+    console.log(tripsByTime.top(Infinity).length);
+
+    //How many trips in 2012 started from Northeastern (station id 5)?
+    tripsByAge.filterAll();
+    tripsByStartStation.filter('5');
+    console.log(tripsByTime.top(Infinity).length);
+
+    //Clear all filters
+    tripsByStartStation.filterAll();
+    tripsByTime.filterAll();
+
+
+    //Group
+    //Combine trips into 10-year age groups; how many trips in each age group?
+
+
+
+
+
+    tripsByAgeGroup = tripsByAge.group(function(d){return Math.floor(d/10)});
+    console.log(tripsByAgeGroup.all());
 
 }
 
@@ -17,7 +56,9 @@ function parse(d){
         startTime: parseDate(d.start_date),
         endTime: parseDate(d.end_date),
         startStation: d.strt_statn,
-        endStation: d.end_statn
+        endStation: d.end_statn,
+        userAge: d.birth_date?parseDate(d.start_date).getFullYear()- (+d.birth_date):0,
+        gender:d.gender? d.gender:"Unknown"
     }
 }
 
